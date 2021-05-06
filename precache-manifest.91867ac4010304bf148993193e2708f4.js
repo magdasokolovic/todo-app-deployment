e3ad23b7bced3f3bdeec8cc7d1e2bbd5d5433145
magdasokolovic <magda.sokolovic@gmail.com>
2021-05-06 10:11:51 +0200

    first commit

---
+++ precache-manifest.91867ac4010304bf148993193e2708f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d3cee3833ae1053f2179c80b5a19f83",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e1e412e9d8d77f62d0fa",
+    "url": "./static/css/main.b3762764.chunk.css"
+  },
+  {
+    "revision": "059c86e55a5079447f9b",
+    "url": "./static/js/2.054ebbe8.chunk.js"
+  },
+  {
+    "revision": "e1e412e9d8d77f62d0fa",
+    "url": "./static/js/main.19294ed3.chunk.js"
+  },
+  {
+    "revision": "baab4f6995ba25947e6f",
+    "url": "./static/js/runtime-main.f5f70450.js"
+  }
+]);
